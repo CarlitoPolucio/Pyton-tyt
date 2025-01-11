a2d4b8012fc42a7bf51c40e21bd31e2560a4fc7e
Vseolod Bogdashov <fishface6666@mail.ru>
2025-01-12 00:22:50 +0700

    Lesson 5: check #2

--- lesson5/task9.py
+++ lesson5/task9.py
@@ -1,12 +1,24 @@
-value = int(input("How many cards: "))
-cards = range(1, value + 1)
-check = sum(cards)
-checker = 0
+# value = int(input("How many cards: "))
+# cards = range(1, value + 1)
+# check = sum(cards)
+# checker = 0
+#
+# for card in cards:
+#     if card == value:
+#         break
+#     number = int(input("enter the num: "))
+#     checker = number + checker
+# answer = check - checker
+# print(f"{answer} is lost")
 
-for card in cards:
-    if card == value:
-        break
-    number = int(input("enter the num: "))
-    checker = number + checker
-answer = check - checker
+# Оптимизация и полировка
+value = int(input("How many cards: "))
+total_cards_sum = value
+current_cards_sum = 0
+
+for card_num in range(1, value):
+    num = int(input("enter the num: "))
+    current_cards_sum += num
+    total_cards_sum += card_num
+answer = total_cards_sum - current_cards_sum
 print(f"{answer} is lost")
