eacf0cbec5ed9ed2c044d93aa931278db7314718
Vseolod Bogdashov <fishface6666@mail.ru>
2025-01-14 22:57:14 +0700

    Lesson 5: check #6

--- lesson5/task21.py
+++ lesson5/task21.py
@@ -29,6 +29,20 @@ if x >= y:
                 y -= 1
 
 
+# QB, QG = int(input("B: ")), int(input("G: "))
+# DOUBLE_MIN_Q, MAX_Q = min(QB, QG) * 2, max(QB, QG)
+#
+# if DOUBLE_MIN_Q < MAX_Q:
+#     print("Impossible")
+# else:
+#     diff = abs(QB - QG)
+#     if QB > QG:
+#         pattern = "BGB"
+#     else:
+#         pattern = "GBG"
+#     print(pattern * diff + "BG" * (DOUBLE_MIN_Q - MAX_Q))
+
+
 
 
 
