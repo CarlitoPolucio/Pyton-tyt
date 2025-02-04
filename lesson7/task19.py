d9a7edc0be4ee296672d17050a22e8452c9e9799
Vseolod Bogdashov <fishface6666@mail.ru>
2025-02-05 00:58:11 +0700

    Lesson 7: tasks corrected; added recommendations

--- lesson7/task19.py
+++ lesson7/task19.py
@@ -20,6 +20,14 @@ for num in range(1, user_input + 1):
     print("".join(full_output))
     next_num -= 1
 
-
-
-
+# Рефакторинг + оптимизация
+# depth = int(input("Enter the num: "))
+#
+# left_digits = ""
+# right_output = ""
+# width = len("".join((str(i) for i in range(1, depth + 1)))) * 2
+#
+# for i in range(depth, 0, -1):
+#     right_output = str(i) + right_output
+#     left_digits += str(i)
+#     print(left_digits + "." * (width - len(right_output) * 2) + right_output)
