83a88f955d92b447f8b31b49730dad6a7743544b
CarlitoPolucio <doubleperfectfantasy@gmail.com>
2025-03-18 21:40:17 +0700

    some shit is done

--- lesson8/task35.py
+++ lesson8/task35.py
@@ -4,13 +4,13 @@ from math import modf
 def position(x, y):
     shit_x, pos_x = modf(x * 10 + 1)
     shit_y, pos_y = modf(y * 10 + 1)
-    if not int(pos_x) in range(0, 8) or not int(pos_y in range(0, 8)):
+    if not int(pos_x) in range(0, 9) or not int(pos_y in range(0, 9)):
         return "Error"
     return int(pos_x), int(pos_y)
 
 
 def check_point(horse_pos_x, horse_pos_y, point_x, point_y):
-    if point_x or point_y in [horse_pos_x, horse_pos_y]:
+    if point_x in [horse_pos_x, horse_pos_y] or point_y in [horse_pos_x, horse_pos_y]:
         return "You cant"
     elif (point_y + point_x) - (horse_pos_x + horse_pos_y) in [1, -1, 3, -3]:
         return "You can"
@@ -19,10 +19,10 @@ def check_point(horse_pos_x, horse_pos_y, point_x, point_y):
 
 
 def main():
-    horse_x = 0.433
-    horse_y = 0.433
-    p_x = 0.445
-    p_y = 0.556
+    horse_x = 0.755
+    horse_y = 0.765
+    p_x = 0.545
+    p_y = 0.256
     horse_pos_x, horse_poz_y = position(horse_x, horse_y)
     point_pos_x, point_poz_y = position(p_x, p_y)
     print(check_point(horse_pos_x, horse_poz_y, point_pos_x, point_poz_y))
